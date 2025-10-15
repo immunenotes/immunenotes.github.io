427ef18817035e385b9e46453b72d4601fa9077c
marcgab91 <marcgabriel@gmx.net>
2025-10-15 12:31:14 +0200

    Update auth

--- js/encrypted/index.js
+++ js/encrypted/index.js
@@ -1,7 +1,3 @@
 window.protectedPages = [
-  { path: 'athletes/PerformanceHealth_Frauen1Fbb_2.html', title: 'PerformanceHealth_Frauen1Fbb_2', encrypted: 'athletes_PerformanceHealth_Frauen1Fbb_2.html' },
-  { path: 'athletes/PerformanceHealth_Frauen1Fbb_3.html', title: 'PerformanceHealth_Frauen1Fbb_3', encrypted: 'athletes_PerformanceHealth_Frauen1Fbb_3.html' },
-  { path: 'athletes/female_cycle_and_exercise.html', title: 'female_cycle_and_exercise', encrypted: 'athletes_female_cycle_and_exercise.html' },
-  { path: 'athletes/PerformanceHealth_Frauen1Fbb_1.html', title: 'PerformanceHealth_Frauen1Fbb_1', encrypted: 'athletes_PerformanceHealth_Frauen1Fbb_1.html' },
   { path: 'athletes.html', title: 'athletes', encrypted: 'athletes.html' },
 ];
